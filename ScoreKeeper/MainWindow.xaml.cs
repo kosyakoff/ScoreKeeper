78bcd389f20648965e1f0367cf8f4234bfa0f391
Kosyakoff <Kosyakoff@CUCUMBER>
2018-05-11 22:58:41 +0700

    Initial release

---
+++ ScoreKeeper/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using ScoreKeeper.Model;
+using ScoreKeeper.ViewModels;
+using ScoreKeeper.Views;
+
+namespace ScoreKeeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += OnLoaded;
+        }
+
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            this.DataContext = new MainWindowViewModel(NavigateToView);
+        }
+
+        private void NavigateToView(UserControl view)
+        {
+            MainArea.Content = view;
+        }
+        
+    }
+}
